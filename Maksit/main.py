919c6fe2dae63b2561fc8bcdd59ae38a315d8c4c
True Leifee!!! <102321360+neeLeifee@users.noreply.github.com>
2024-09-23 16:58:33 +0300

    gotta choose

--- Maksit/main.py
+++ Maksit/main.py
@@ -11,11 +11,22 @@ def gamefield_gen_2D(GameNums_2D): # Работает
         for j in i:
             print(f'   {j}', end = '   ')
         print('\n')
+        
+
+def gamefield_gen_2D_second(GameNums_2D): # Работает
+    print('\n-------------------',end='')
+    print(f'\n|  {GameNums_2D[0][0]}  |  {GameNums_2D[0][1]}  |  {GameNums_2D[0][2]}  |',end='')
+    print('\n-------------------',end='')
+    print(f'\n|  {GameNums_2D[1][0]}  |  {GameNums_2D[1][1]}  |  {GameNums_2D[1][2]}  |',end='')
+    print('\n-------------------',end='')
+    print(f'\n|  {GameNums_2D[2][0]}  |  {GameNums_2D[2][1]}  |  {GameNums_2D[2][2]}  |',end='')
+    print('\n-------------------')
     
 
 # test
         
 gamefield_gen_2D(GameNums_2D)
+gamefield_gen_2D_second(GameNums_2D)
 
 
 
